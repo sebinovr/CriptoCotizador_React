54b753da069a06d2f6972d089cc0ebf370a56276
Sebastian Venegas <115212791+sebinovr@users.noreply.github.com>
2023-01-17 14:57:01 +0100

    Add files via upload

---
+++ src/data/monedas.js
@@ -0,0 +1,12 @@
+    //Arreglo estatico
+const monedas = [
+    { id: "USD", nombre: "Dolar de Estados Unidos"},
+    { id: "MXN", nombre: "Peso Mexicano"},
+    { id: "EUR", nombre: "Euro"},
+    { id: "GBP", nombre: "Libra Esterlina"},
+    { id: "CLP", nombre: "Peso Chileno"}
+]
+
+export {
+    monedas
+}
